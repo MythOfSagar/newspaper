2f151e5cb6962f749d7698a045a470e81a0748fc
Sagar Wankhade <105979852+Sagar1079@users.noreply.github.com>
2022-12-04 11:29:20 +0530

    Update NewsPaper.jsx

--- src/component/NewsPaper.jsx
+++ src/component/NewsPaper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewsPaper = ({thumbNail,paperURL,title}) => {
   return (
-    <><a target="_blank" href={paperURL}><div
+    <><a rel="noreferrer" target="_blank" href={paperURL}><div
       style={{width:"99%",marginTop:"30px",border:"2px solid red",borderRadius:"50px"}}>
         <img style={{width:"300px"}} src={thumbNail} alt="" />
     </div></a></>
